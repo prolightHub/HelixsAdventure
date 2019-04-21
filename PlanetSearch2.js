be57c8b4b79beb28f7e0e20ac262a0a5b65ae703
ProlightHub <evan.sweet@outlook.com>
2019-04-21 12:29:50 -0600

    Added extras

--- PlanetSearch2.js
+++ PlanetSearch2.js
@@ -772,7 +772,7 @@ var sketch = function(processing) /*Wrapper*/
 var game = {
     fps : 60, 
     loadFps : 160,
-    gameState : "play", //Default = "start"
+    gameState : "start", //Default = "start"
     version : "v0.8.6 beta",
     fpsType : "manual", //Default = "manual"
     debugMode : true, //Turn this to true to see the fps
@@ -786,7 +786,7 @@ var game = {
     }
 };
 var levelInfo = {
-    level : "generated", //Default = "intro"
+    level : "intro", //Default = "intro"
     xPos : 0,
     yPos : 0,
     width : width,
